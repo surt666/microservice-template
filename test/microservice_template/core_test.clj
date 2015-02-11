0a93326393c17dd49ab00296b81123e38c9cfd81
Steen Larsen <steen666@gmail.com>
2015-02-11 17:19:43 +0100

    fixed tests

--- test/microservice_template/core_test.clj
+++ test/microservice_template/core_test.clj
@@ -4,13 +4,27 @@
             [ring.mock.request :as mock]))
 
 (expect
- (dissoc (handler (mock/request :get "/service/12")) :headers)
+ (let [m (dissoc (handler (mock/header (mock/request :get "/service/12") "accept" "application/vnd.skm+transit-json;verbose")) :headers)]
+   (assoc m :body (slurp (:body m))))
  {:status  200
 ; :headers {"content-type" "text/plain"}
-  :body    "{:foo \"bar\"}"})
+  :body    "{\"foo\":\"bar\"}"})
 
 (expect
- (dissoc (handler (mock/request :options "/service")) :headers)
+ (dissoc (handler (mock/header (mock/request :options "/service") "accept" "application/vnd.skm+transit-json")) :headers)
  {:status  200
 ; :headers {"content-type" "text/plain"}
-  :body    "{:foo \"bar\"}"})
+  :body    (str
+"Method: See allow
+Accept: See accept header
+Content-Type: See accept header\n"
+"Data in body should have the following format\n"
+"    {
+       \"billproduct/productid\": \"12334\",
+       \"billproduct/name\": \"somename\",
+       \"billproduct/rollover-date\": \"31-01-2015:12:00:00\",
+       \"billproduct/rollover-product\": \"9393993\",
+       \"billproduct/services\": [\"serv1\", \"serv2\"],
+       \"billproduct/usage\": [\"usage1\"],
+       \"billproduct/data\": [{\"billproduct.data/key\": \"somekey\", \"billproduct.data/value\": \"somevalue\"}]
+     }")})
